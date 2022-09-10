133a65ead4fa4b7681c3f1f952f4173118202829
Bruno Tiago <brunotiago2000@hotmail.com>
2022-09-10 14:10:45 -0300

    jsx

--- src/favorite-foods/routes/index.js
+++ src/favorite-foods/routes/index.js
@@ -1,6 +1,6 @@
-import HomeView from '../views/Home'
-import AddView from '../views/Add';
-import UpdateView from '../views/Update';
+import HomeView from '../views/Home.jsx'
+import AddView from '../views/Add.jsx';
+import UpdateView from '../views/Update.jsx';
 
 export {HomeView, AddView, UpdateView};
 
